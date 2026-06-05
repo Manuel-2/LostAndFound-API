275b3a491209a0bd88f033d538abeaa8c8c4ae88
Manuel Peñaloza <25lopezm25@gmail.com>
2026-06-04 19:31:44 -0700

    Varias sessiones mobiles

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if ($user && Hash::check($request->password, $user->password)) {
-            $user->tokens()->delete();
+            // $user->tokens()->delete();
 
             return $user->createToken($user->name)->plainTextToken;
         } else {
@@ -31,7 +31,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->tokens()->delete();
+        // $request->user()->tokens()->delete();
         return response()->json([
             'message' => "Sesión cerrada correctamente."
         ]);
