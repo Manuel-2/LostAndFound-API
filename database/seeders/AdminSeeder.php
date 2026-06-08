bbaa7ce042568dfd0592b1709f5accac92694b36
Manuel-2 <25lopezm25@gmail.com>
2026-06-07 22:43:16 -0700

    fix seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,10 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Pest\Support\Str;
+use Illuminate\Support\Str;
 use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
